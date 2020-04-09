44f6f857b3197a92ac067687c39703863e31867f
Piru Perampalam <23018824+developerpiru@users.noreply.github.com>
2020-04-09 00:46:21 -0400

    Add files via upload

--- installpkgs.R
+++ installpkgs.R
@@ -1,44 +1,47 @@
-installPkgs <- function(package_name, bioc){
-  if (bioc == FALSE)
-    install.packages(package_name)
-  else if (bioc == TRUE) #install using Bioconductor package manager
-    BiocManager::install(package_name)
+library(methods)
+
+installCran <- function(package_name){
+  install.packages(package_name)
+}
+
+installBioc <- function(package_name){
+  BiocManager::install(package_name)
 }
 
 ## load required libraries
-installPkgs("shiny")
-installPkgs("shinydashboard")
-installPkgs("shinyWidgets")
+installCran("shiny")
+installCran("shinydashboard")
+installCran("shinyWidgets")
 
 #cran packages
-installPkgs("BiocManager")
-installPkgs("colourpicker")
-installPkgs("data.table")
-installPkgs("devtools") # to install from github
-installPkgs("DT")
-installPkgs("ggplot2")
-installPkgs("ggpubr")
-installPkgs("ggrepel")
-installPkgs("ggraph")
-installPkgs("gridExtra")
-installPkgs("pheatmap")
-installPkgs("RColorBrewer")
-installPkgs("scales")
-installPkgs("shiny")
-installPkgs("shinydashboard")
-installPkgs("shinyjqui")
-installPkgs("shinyWidgets")
-installPkgs("shinycssloaders")
-installPkgs("circlize")
+installCran("BiocManager")
+installCran("colourpicker")
+installCran("data.table")
+installCran("devtools") # to install from github
+installCran("DT")
+installCran("ggplot2")
+installCran("ggpubr")
+installCran("ggrepel")
+installCran("ggraph")
+installCran("gridExtra")
+installCran("pheatmap")
+installCran("RColorBrewer")
+installCran("scales")
+installCran("shiny")
+installCran("shinydashboard")
+installCran("shinyjqui")
+installCran("shinyWidgets")
+installCran("shinycssloaders")
+installCran("circlize")
 
 # #Bioconductor packages
-installPkgs("DESeq2", bioc == TRUE)
-installPkgs("vsn", bioc == TRUE)
-installPkgs("apeglm", bioc == TRUE)
-installPkgs("org.Hs.eg.db", bioc == TRUE)
-installPkgs("org.Mm.eg.db", bioc == TRUE)
-installPkgs("ReactomePA", bioc == TRUE)
-installPkgs("enrichplot", bioc == TRUE)
+installBioc("DESeq2")
+installBioc("vsn")
+installBioc("apeglm")
+installBioc("org.Hs.eg.db")
+installBioc("org.Mm.eg.db")
+installBioc("ReactomePA")
+installBioc("enrichplot")
 
 # #GitHub packages
 devtools::install_github("kevinblighe/EnhancedVolcano")
